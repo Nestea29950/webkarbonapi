680363262bf79b8beece24defd3f519e51018aed
Nestea <72440100+Nestea29950@users.noreply.github.com>
2023-10-13 15:49:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,64 +14,54 @@ app.get("/api", async function (req, res) {
 
   const origin = req.get('Origin');
 
-      if(origin == "https://www.webkarbon.bzh" || origin == "https://www.webkarbon.fr" && url != "https://api.webkarbon.fr" && url != "https://api.webkarbon.fr/"){
-
-      if (activeWorkers < MAX_WORKERS) {
-
+  if ((origin === "https://www.webkarbon.bzh" || origin === "https://www.webkarbon.fr") && url !== "https://api.webkarbon.fr" && url !== "https://api.webkarbon.fr/") {
+    if (activeWorkers < MAX_WORKERS) {
       createWorkerThread(url, res);
-
     } else {
-
       // Ajouter l'appel d'API à la file d'attente
-
       apiQueue.push({ url, res });
-
+      console.log("Demande mise en file d'attente :", url);
     }
-      }
-      else{
-
-        res.send("erreur");
-
-      }
-
+  } else {
+    res.send("erreur");
+  }
 });
 
 // Création d'un worker thread
-// Création d'un worker thread
 function createWorkerThread(url, res) {
-
   const worker = new Worker('./worker.js', { workerData: url });
+  console.log("Nouveau worker créé pour :", url);
 
   activeWorkers++;
 
   // Événement de réception de message du thread
   worker.on('message', audits => {
-    console.log(audits);
-    if(audits == false){
+    console.log("Réponse reçue pour :", url);
+    if (audits === false) {
       res.status(400).send("Problème url");
-    }
-    else{
+    } else {
       res.send({ audits });
     }
 
     if (apiQueue.length > 0) {
       // Récupérer le prochain appel d'API dans la file d'attente
       const nextApiCall = apiQueue.shift();
+      console.log("Traitement de la demande en file d'attente pour :", nextApiCall.url);
       createWorkerThread(nextApiCall.url, nextApiCall.res);
     } else {
       activeWorkers--;
+      console.log("Worker terminé pour :", url);
 
       // S'il n'y a plus de travailleurs actifs, cela peut être un bon moment pour rediriger
       // les demandes de la file d'attente vers de nouveaux workers, le cas échéant.
-
       if (activeWorkers === 0 && apiQueue.length > 0) {
         const nextApiCall = apiQueue.shift();
+        console.log("Traitement de la demande en file d'attente pour :", nextApiCall.url);
         createWorkerThread(nextApiCall.url, nextApiCall.res);
       }
     }
   });
 }
-
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
