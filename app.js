b6e84eee69b5fe74a8ec5806c85c6996763464d8
Nestea <72440100+Nestea29950@users.noreply.github.com>
2023-10-13 14:39:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,36 +13,57 @@ app.get("/api", async function (req, res) {
   const url = req.query.url;
 
   const origin = req.get('Origin');
+
       if(origin == "https://www.webkarbon.bzh" || origin == "https://www.webkarbon.fr" && url != "https://api.webkarbon.fr" && url != "https://api.webkarbon.fr/"){
+
       if (activeWorkers < MAX_WORKERS) {
+
       createWorkerThread(url, res);
+
     } else {
+
       // Ajouter l'appel d'API à la file d'attente
+
       apiQueue.push({ url, res });
+
     }
       }
       else{
+
         res.send("erreur");
+
       }
-  
+
 });
 
 // Création d'un worker thread
 function createWorkerThread(url, res) {
+
   const worker = new Worker('./worker.js', { workerData: url });
+
   activeWorkers++;
 
   // Événement de réception de message du thread
   worker.on('message', audits => {
+
     res.send({ audits });
 
     if (apiQueue.length > 0) {
+
       // Récupérer le prochain appel d'API dans la file d'attente
+
       const nextApiCall = apiQueue.shift();
+      
       createWorkerThread(nextApiCall.url, nextApiCall.res);
-    } else {
+
+    } 
+    
+    else {
+
       activeWorkers--;
+
     }
+
   });
 }
 
